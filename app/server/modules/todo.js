a7ebda96213f6aaef117b39b99eb35ea2a190aac
tomayoola <tom.ayoola@abehr.com>
2017-11-01 16:09:18 +0000

    added buttons to todo items

--- app/server/modules/todo.js
+++ app/server/modules/todo.js
@@ -5,6 +5,7 @@
 var mongoose = require ('mongoose');
 module.exports = mongoose.model('Todo', {
     text : String,
+    tags : [String],
     user: String,
     date_created: Date,
     done : Boolean
