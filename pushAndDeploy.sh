3722969e90d0c0b1199589878303895cd7243bc2
tomayoola <tom.ayoola@abehr.com>
2017-11-02 17:48:28 +0000

    deploy

--- pushAndDeploy.sh
+++ pushAndDeploy.sh
@@ -1,4 +1,6 @@
-
+git add -A
+git commit -m "deploy"
+git push
 docker build --no-cache -t tomayoola/notebook .
 docker push tomayoola/notebook:latest
 az webapp restart --name uclteam17 --resource-group notebookResourceGroup
