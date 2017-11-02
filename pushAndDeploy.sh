e444881f1e5229aa88ca812471c599752788c247
tomayoola <tom.ayoola@abehr.com>
2017-11-02 20:34:40 +0000

    switched docker image

--- pushAndDeploy.sh
+++ pushAndDeploy.sh
@@ -1,6 +1,6 @@
 git add -A
 git commit -m "deploy"
 git push
-docker build --no-cache -t tomayoola/notebook .
-docker push tomayoola/notebook:latest
+docker build --no-cache -t tomayoola/notebook:staging .
+docker push tomayoola/notebook:staging
 az webapp restart --name uclteam17 --resource-group notebookResourceGroup
